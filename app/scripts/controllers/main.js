2166abc23834638e1f39ec798519f25050cd9419
Brian Dunnette <brian@dunnette.us>
2014-01-19 15:42:37 -0600

    initial version using imageserver tiles

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,4 +7,20 @@ angular.module('synoviumApp')
       'AngularJS',
       'Karma'
     ];
+    
+    //For an Aperio server, tile URL will be something like: http://images.pathology.umn.edu/imageserver/Conferences/Residents/SurgUnknowns/2014-01-23/55687.svs?x+y+256+256+z
+    var tilesDict = {
+        two: {
+            url: "http://images.pathology.umn.edu/imageserver/Conferences/Residents/SurgUnknowns/2014-01-23/55687.svs?{x}+{y}+256+256+{z}",
+        },
+        one: {
+            url: "http://images.pathology.umn.edu/imageserver/Conferences/Residents/SurgUnknowns/2014-01-23/55686.svs?{x}+{y}+256+256+{z}",
+        }
+    };
+      
+    angular.extend($scope, {
+        tiles:{
+            url: "http://images.pathology.umn.edu/imageserver/Conferences/Residents/SurgUnknowns/2014-01-23/55687.svs?{x}+{y}+256+256+{z}",
+        },
+    });
   });
